c10d8a578baed18e1708fa14b310b6668932b2c5
Lukas Nizik <elejdor@gmail.com>
2014-06-15 16:15:00 +0200

    buttony dla miejsc na sali kinowej

--- trunk/KinoManagement/src/MyCinemaCustomerView.java
+++ trunk/KinoManagement/src/MyCinemaCustomerView.java
@@ -1,19 +1,33 @@
 import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 import javax.swing.JPanel;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.Rectangle;
+
 import javax.swing.BoxLayout;
+
 import java.awt.Font;
+
 import javax.swing.SwingConstants;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.sql.*;
+
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class MyCinemaCustomerView extends CinemaContentPanel
 {
@@ -24,6 +38,12 @@ public class MyCinemaCustomerView extends CinemaContentPanel
 	private JPanel tabShows;
 	private JPanel tabTickets;
 	private JPanel tabFinancials;
+	
+	//tickets tab
+	public JScrollPane shows_scrollList;
+	public JList<String> shows_showsList;
+	public ArrayList<ShowsRow> showsList;
+	public ArrayList<JButton> places;
 	
 	public MyCinemaCustomerView(MyCinemaController myCinemaController)
 	{
@@ -47,8 +67,34 @@ public class MyCinemaCustomerView extends CinemaContentPanel
 	{
 		tabtickets = new JPanel();
 		tabtickets.setName("Tickets");
+		tabtickets.setLayout(null);
 		tabtickets.setBackground(this.getBackground().brighter());
 		jTabbedPane.add(tabtickets);
+		
+		this.shows_showsList = new JList<String>();
+		this.shows_showsList.setFont(new Font("Sylfaen", Font.PLAIN, 14));
+		this.shows_scrollList = new JScrollPane();
+		this.shows_scrollList.setBounds(50, 50, 350, 270);
+		this.shows_scrollList.setVisible(true);
+		this.shows_scrollList.setViewportView(shows_showsList);
+		tabtickets.add(shows_scrollList);
+		this.showsList = this.myCinemaController.GetShowsList();
+		this.shows_showsList.setListData(MyCinemaController.ShowsRowListToArray(this.showsList));
+		ListSelectionListener actionIndexChanged = new ListSelectionListener() 
+		{
+			public void valueChanged(ListSelectionEvent arg0) 
+			{
+				if(MyCinemaSellerView.sellerView.users_userList.getSelectedIndex() != -1)
+				{
+					
+				} else{
+					
+				}
+			}
+		};
+		this.shows_showsList.addListSelectionListener(actionIndexChanged);
+		
+		createPlaces(7, 5, 420, 50, 50, 30, 2);
 	}
 	private void initializeTabShows(JTabbedPane jTabbedPane)
 	{
@@ -63,5 +109,26 @@ public class MyCinemaCustomerView extends CinemaContentPanel
 		tabFinancials.setName("Financials");
 		tabFinancials.setBackground(this.getBackground().brighter());
 		jTabbedPane.add(tabFinancials);
+	}
+	
+	private void fillPlacesForShow()
+	{
+		
+	}
+	
+	private void createPlaces(int rows, int cols, int startPosX, int startPosY, int width, int height, int margins)
+	{
+		places = new ArrayList<JButton>();
+		for (int r = 0; r < rows; r++)
+		{
+			for (int c = 0; c < cols; c++)
+			{
+				JButton tmpBtn = new JButton(Integer.toString(c+cols*r));
+				tmpBtn.setBounds(startPosX + c * (width+margins), startPosY + r* (height + margins), width, height);
+				tabtickets.add(tmpBtn);
+				places.add(tmpBtn);
+			}
+				
+		}
 	}
 }
